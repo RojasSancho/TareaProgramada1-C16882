9811b8960213c9515ff14f854c2f4f5098b9ed8c
RojasSancho <hermes.rojassancho@ucr.ac.cr>
2022-05-01 17:10:31 -0600

    Avance en tests de servicioPostal

--- TareaProgramada1/tests/tests_envioserviciopostal_calculoenvio.cpp
+++ TareaProgramada1/tests/tests_envioserviciopostal_calculoenvio.cpp
@@ -14,4 +14,40 @@ namespace
         //
         EXPECT_FLOAT_EQ(esperada, actual);
     }
+
+    TEST(Test_EnvioServicioPostal_CalculoEnvio, Test_De0A3KgYSegundaClase) {
+        //
+        EnvioServicioPostal envioServicioPostal(450, 1, 2); 
+        
+        //
+        float actual = envioServicioPostal.calculoEnvio();
+        float esperada = 12.6;
+
+        //
+        EXPECT_FLOAT_EQ(esperada, actual);
+    }
+
+    TEST(Test_EnvioServicioPostal_CalculoEnvio, Test_De0A3KgYTerceraClase) {
+        //
+        EnvioServicioPostal envioServicioPostal(450, 1, 3); 
+        
+        //
+        float actual = envioServicioPostal.calculoEnvio();
+        float esperada = 5.4;
+
+        //
+        EXPECT_FLOAT_EQ(esperada, actual);
+    }
+
+    TEST(Test_EnvioServicioPostal_CalculoEnvio, Test_De4A8KgYPrimeraClase) {
+        //
+        EnvioServicioPostal envioServicioPostal(450, 7, 1); 
+        
+        //
+        float actual = envioServicioPostal.calculoEnvio();
+        float esperada = 202.5;
+
+        //
+        EXPECT_FLOAT_EQ(esperada, actual);
+    }
 }
