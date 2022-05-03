a2f06ded3a13d691e79dba8e4b06045534133499
RojasSancho <hermes.rojassancho@ucr.ac.cr>
2022-05-02 20:25:12 -0600

    Comentarios en tests

--- TareaProgramada1/tests/tests_enviofedex_calculoenvio.cpp
+++ TareaProgramada1/tests/tests_enviofedex_calculoenvio.cpp
@@ -3,66 +3,62 @@
 
 namespace
 {
-    TEST(Test_EnvioFedex_CalculoEnvio, Test_DistanciaMayorA500YPesoMenorOIgualA10)
-    
-    {
-        //
+    TEST(Test_EnvioFedex_CalculoEnvio, Test_DistanciaMayorA500YPesoMenorOIgualA10) {
+
+        // Arrange
         EnvioFedEX envioFedEX(650,8);
 
 
-        //
+        // Act
         float actual = envioFedEX.calculoEnvio();
         float esperada = 50.00;
 
-        //
+        // Assert
         EXPECT_FLOAT_EQ(esperada, actual);
 
     }
 
-    TEST(Test_EnvioFedex_CalculoEnvio, Test_DistanciaMayorA500YPesoMayorA10)
-    
-    {
-        //
+    TEST(Test_EnvioFedex_CalculoEnvio, Test_DistanciaMayorA500YPesoMayorA10) {
+
+        // Arrange
         EnvioFedEX envioFedEX(510,17);
 
 
-        //
+        // Act
         float actual = envioFedEX.calculoEnvio();
         float esperada = 60.00;
 
-        //
+        // Assert
         EXPECT_FLOAT_EQ(esperada, actual);
 
     }
 
-    TEST(Test_EnvioFedex_CalculoEnvio, Test_DistanciaMenorOIgualA500YPesoMenorOIgualA10)
-    
-    {
-        //
+    TEST(Test_EnvioFedex_CalculoEnvio, Test_DistanciaMenorOIgualA500YPesoMenorOIgualA10) {
+
+        // Arrange
         EnvioFedEX envioFedEX(370,4);
 
 
-        //
+        // Act
         float actual = envioFedEX.calculoEnvio();
         float esperada = 35.00;
 
-        //
+        // Assert
         EXPECT_FLOAT_EQ(esperada, actual);
 
     }
 
-    TEST(Test_EnvioFedex_CalculoEnvio, Test_DistanciaMenorOIgualA500YPesoMayorA10)
-    
-    {
-        //
+    TEST(Test_EnvioFedex_CalculoEnvio, Test_DistanciaMenorOIgualA500YPesoMayorA10) {
+        
+        // Arrange 
         EnvioFedEX envioFedEX(475,27);
 
 
-        //
+        // Act
         float actual = envioFedEX.calculoEnvio();
         float esperada = 45.00;
 
-        //
+        // Assert
         EXPECT_FLOAT_EQ(esperada, actual);
 
     }
--- TareaProgramada1/tests/tests_envioserviciopostal_calculoenvio.cpp
+++ TareaProgramada1/tests/tests_envioserviciopostal_calculoenvio.cpp
@@ -4,111 +4,119 @@
 namespace
 {
     TEST(Test_EnvioServicioPostal_CalculoEnvio, Test_De1KgYPrimeraClase) {
-        //
+
+        // Arrange
         EnvioServicioPostal envioServicioPostal(450, 1, 1); 
         
-        //
+        // Act
         float actual = envioServicioPostal.calculoEnvio();
         float esperada = 135.00;
 
-        //
+        // Assert
         EXPECT_FLOAT_EQ(esperada, actual);
     }
 
     TEST(Test_EnvioServicioPostal_CalculoEnvio, Test_De2KgYSegundaClase) {
-        //
+
+        // Arrange
         EnvioServicioPostal envioServicioPostal(450, 2, 2); 
         
-        //
+        // Act
         float actual = envioServicioPostal.calculoEnvio();
         float esperada = 12.6;
 
-        //
+        // Assert
         EXPECT_FLOAT_EQ(esperada, actual);
     }
 
     TEST(Test_EnvioServicioPostal_CalculoEnvio, Test_De3KgYTerceraClase) {
-        //
+
+        // Arrange
         EnvioServicioPostal envioServicioPostal(450, 3, 3); 
         
-        //
+        // Act
         float actual = envioServicioPostal.calculoEnvio();
         float esperada = 5.4;
 
-        //
+        // Assert
         EXPECT_FLOAT_EQ(esperada, actual);
     }
 
     TEST(Test_EnvioServicioPostal_CalculoEnvio, Test_De4KgYPrimeraClase) {
-        //
+
+        // Arrange
         EnvioServicioPostal envioServicioPostal(450, 4, 1); 
         
-        //
+        // Act
         float actual = envioServicioPostal.calculoEnvio();
         float esperada = 202.5;
 
-        //
+        // Assert
         EXPECT_FLOAT_EQ(esperada, actual);
     }
 
     TEST(Test_EnvioServicioPostal_CalculoEnvio, Test_De5KgYSegundaClase) {
-        //
+
+        // Arrange
         EnvioServicioPostal envioServicioPostal(450, 5, 2); 
         
-        //
+        // Act
         float actual = envioServicioPostal.calculoEnvio();
         float esperada = 23.85;
 
-        //
+        // Assert
         EXPECT_FLOAT_EQ(esperada, actual);
     }
 
     TEST(Test_EnvioServicioPostal_CalculoEnvio, Test_De6KgYTerceraClase) {
-        //
+
+        // Arrange
         EnvioServicioPostal envioServicioPostal(450, 6, 3); 
         
-        //
+        // Act
         float actual = envioServicioPostal.calculoEnvio();
         float esperada = 5.4;
 
-        //
+        // Assert
         EXPECT_FLOAT_EQ(esperada, actual);
     }
 
     TEST(Test_EnvioServicioPostal_CalculoEnvio, Test_De9KgYPrimeraClase) {
-        //
+
+        // Arrange
         EnvioServicioPostal envioServicioPostal(450, 9, 1); 
         
-        //
+        // Act
         float actual = envioServicioPostal.calculoEnvio();
         float esperada = 270.00;
 
-        //
+        // Assert
         EXPECT_FLOAT_EQ(esperada, actual);
     }
 
     TEST(Test_EnvioServicioPostal_CalculoEnvio, Test_De10KgYSegundaClase) {
-        //
+
+        // Arrange
         EnvioServicioPostal envioServicioPostal(450, 10, 2); 
         
-        //
+        // Act
         float actual = envioServicioPostal.calculoEnvio();
         float esperada = 33.75;
 
-        //
+        // Assert
         EXPECT_FLOAT_EQ(esperada, actual);
     }
 
     TEST(Test_EnvioServicioPostal_CalculoEnvio, Test_De11KgYTerceraClase) {
-        //
+
+        // Arrange
         EnvioServicioPostal envioServicioPostal(450, 11, 3); 
         
-        //
+        // Act
         float actual = envioServicioPostal.calculoEnvio();
         float esperada = 5.4;
 
-        //
+        // Assert
         EXPECT_FLOAT_EQ(esperada, actual);
     }
 }
-
