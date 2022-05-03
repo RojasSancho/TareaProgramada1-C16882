1e62e4e4916ce02701e8f5970baefc71f3757020
RojasSancho <hermes.rojassancho@ucr.ac.cr>
2022-05-02 21:00:39 -0600

    Pruebas de delete

--- TareaProgramada1/src/main.cpp
+++ TareaProgramada1/src/main.cpp
@@ -8,30 +8,34 @@ using namespace std;
 
 int main() {
 
-    vector<Envio *> envios;
+    vector<Envio *> arregloDeEnvios;
 
     EnvioFedEX* envioPorFedEX1 = new EnvioFedEX(550, 6); // Envio por FedEX de 550 km y con peso de 6 kg
-    envios.push_back(envioPorFedEX1);
+    arregloDeEnvios.push_back(envioPorFedEX1);
 
     EnvioFedEX* envioPorFedEX2 = new EnvioFedEX(420, 27); // Envio por FedEX de 420 km y con peso de 27 kg
-    envios.push_back(envioPorFedEX2);
+    arregloDeEnvios.push_back(envioPorFedEX2);
 
     EnvioServicioPostal* envioPorServicioPostal1 = new EnvioServicioPostal(470, 2, 1); // Envio por Servicio Postal de primera clase de 470 km y con peso de 2 kg
-    envios.push_back(envioPorServicioPostal1);
+    arregloDeEnvios.push_back(envioPorServicioPostal1);
 
     EnvioServicioPostal* envioPorServicioPostal2 = new EnvioServicioPostal(470, 6, 1); // Envio por Servicio Postal de primera clase de 470 km y con peso de 6 kg
-    envios.push_back(envioPorServicioPostal2);
+    arregloDeEnvios.push_back(envioPorServicioPostal2);
 
     EnvioServicioPostal* envioPorServicioPostal3 = new EnvioServicioPostal(470, 12, 1); // Envio por Servicio Postal de primera clase de 470 km y con peso de 12 kg
-    envios.push_back(envioPorServicioPostal3);
+    arregloDeEnvios.push_back(envioPorServicioPostal3);
 
     double totalMontoEnvios = 0;
 
-    for (Envio* envio : envios) {
+    for (Envio* envio : arregloDeEnvios) {
 
-        totalMontoEnvios += envio->calculoEnvio();     
+        totalMontoEnvios += envio->calculoEnvio();   
+         
     }
 
     std::cout << "El monto total final de los envios es de: " << totalMontoEnvios << std::endl;
+
+    delete envioPorServicioPostal3;
+    delete envioPorFedEX1;
 
 }
